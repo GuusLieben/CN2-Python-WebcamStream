7cd17b4280c70158631d0885155031cf7e33039a
Guus Lieben <guus@xendox.com>
2020-05-22 12:22:15 +0200

    Include OpenCV stream from ffmpeg pipe, add pause/stop functionality

--- client.py
+++ client.py
@@ -1,18 +1,16 @@
 from __future__ import division
-
 import subprocess as sp
-
 import cv2
+import numpy
 
-# Paused stream image
 path = r'C:\Users\Guus Lieben\Pictures\dab_stream.png'
-image = cv2.imread(path)  # Use the predefined image as paused image
+paused_image = cv2.imread(path)
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (50, 50)
 fontScale = 0.75
 color = (0, 0, 255)
 thickness = 2
-pause_img = cv2.putText(image, 'Stream paused', org, font,
+pause_img = cv2.putText(paused_image, 'Stream paused', org, font,
                         fontScale, color, thickness, cv2.LINE_AA)
 
 webcam_name = 'Logitech HD Webcam C525'
@@ -20,8 +18,7 @@ webcam_name = 'Logitech HD Webcam C525'
 # FFMPEG command
 ffmpeg = 'ffmpeg'
 source = 'video={}'.format(webcam_name)
-temp_folder = 'D:\CN2PyStream\\temp'
-
+stream_id = str(hash(sp))
 
 def run_ffmpeg():
     command = [
@@ -34,35 +31,48 @@ def run_ffmpeg():
         "-preset", "ultrafast",
         "-tune", "zerolatency",
         "-f", "flv",  # output format
-        "rtmp://localhost/live/stream_id",
+        "rtmp://localhost/live/{}".format(stream_id),
         "-y",
-        "-r", "1",
+        "-r", "24",
         "-pix_fmt", "bgr24",
         "-vcodec", "rawvideo",
         "-an",
-        "-f", "yuv4mpegpipe",
-        temp_folder
+        "-f", "image2pipe",
+        "-"
     ]
     return sp.Popen(command, stdout=sp.PIPE, bufsize=10)
 
 
-def run_cv(ffmpeg_process):
-    cap = cv2.VideoCapture(temp_folder)
-
+def run():
+    pipe = run_ffmpeg()
+    paused = False
     while True:
-        if cap.isOpened():
-            _, frame = cap.read()
-            cv2.imshow('Preview', frame)
+        if not paused:
+            raw_frame = pipe.stdout.read(640 * 360 * 3)
+            frame = numpy.frombuffer(raw_frame, dtype='uint8')
+            frame = frame.reshape((360, 640, 3))
+            cv2.putText(frame, 'ID {}'.format(stream_id), org, font,
+                        fontScale, color, thickness, cv2.LINE_AA)
+            if frame is not None:
+                cv2.imshow('Preview', frame)
+        elif paused:
+            cv2.imshow('Preview', pause_img)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        key = cv2.waitKey(1)
+        if key == 27:  # Esc
+            break
+        elif key == 32:  # Space
+            paused = not paused
+
+        if paused and pipe is not None:
+            pipe.kill()
+            pipe = None
+        elif not paused and pipe is None:
+            pipe = run_ffmpeg()
 
     cv2.destroyAllWindows()
-
-
-def run():
-    ffmpeg_process = run_ffmpeg()
-    run_cv(ffmpeg_process)
+    if pipe is not None:
+        pipe.kill()
 
 
 if __name__ == "__main__":
